760da5b5e6a3760c596b0ab46ee5e24daab59e49
kkssry <kkssry@naver.com>
2019-02-04 18:25:26 +0900

    깊이/너비 우선탐색(DFS/BFS) 타겟너버

---
+++ src/test/java/programmers/q43165Test.java
@@ -0,0 +1,15 @@
+package programmers;
+
+import org.junit.Test;
+
+public class q43165Test {
+
+    @Test
+    public void name() {
+
+        q43165 asdf = new q43165();
+        int[] numbers = { 1,1,1,1,1};
+        System.out.println(asdf.solution(numbers,3));
+    }
+
+}
