f31f30e02d5d22ce8b940fe94cbd945041e53997
kkssry <kkssry@naver.com>
2019-02-12 00:58:52 +0900

    map을 이용한 문제

--- src/main/java/programmers/q49993/Solution.java
+++ src/main/java/programmers/q49993/Solution.java
@@ -9,16 +9,30 @@ public class Solution {
         List<String> list = new ArrayList<>();
 
         String[] skills = skill.split("");
+
         for (int i = 0; i < skill_trees.length; i++) {
             list.clear();
-            if (skill_trees[i].contains(skills[0])) {
+            if (!skill_trees[i].contains(skills[0])) {
                 continue;
             }
-            for (int j = 0; j < skills.length; j++) {
-                list.add(skill_trees[i].substring(skill_trees[i].indexOf(skills[j])));
 
+            for (int j = 0; j < skill_trees[i].length(); j++) {
+                String[] arr = skill_trees[i].split("");
+                if (skill.contains(arr[j])) {
+                    list.add(arr[j]);
+                }
             }
-            
+            System.out.println(list);
+            for (int k = 0; k < list.size(); k++) {
+                if (!list.get(k).equals(skills[k])) {
+                    break;
+                }
+                if (list.get(k).equals(skills[k])) {
+                    if (k == list.size()-1) {
+                        answer++;
+                    }
+                }
+            }
         }
 
         return answer;
