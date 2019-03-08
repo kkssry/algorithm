f8b950942d769e7722b32a35eead0f1c0a08e37d
kkssry <kkssry@naver.com>
2019-03-08 20:13:03 +0900

    정수 내림차순

---
+++ src/test/java/programmers/q12933/SolutionTest.java
@@ -0,0 +1,20 @@
+package programmers.q12933;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+    @Test
+    public void mainTest() {
+        Solution sol = new Solution();
+        System.out.println(sol.solution(118372));
+    }
+
+    @Test
+    public void 문자열더하기숫자() {
+        String a = "";
+        int b = 1;
+        System.out.println(a+b);
+    }
+}
