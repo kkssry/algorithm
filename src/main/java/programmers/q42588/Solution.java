8e2673b7a850d28f69df01cb4a272b80d3b4f4ec
kkssry <kkssry@naver.com>
2019-03-12 12:13:18 +0900

    탑

--- src/main/java/programmers/q42588/Solution.java
+++ src/main/java/programmers/q42588/Solution.java
@@ -1,4 +1,24 @@
 package programmers.q42588;
 
 public class Solution {
+    public int[] solution(int[] heights) {
+        int[] answer = {};
+
+        answer = new int[heights.length];
+
+        for (int i = heights.length - 1; i >= 0; i--) {
+            if (i == 0) {
+                break;
+            }
+
+            for (int j = i - 1; j >= 0; j--) {
+                if (heights[i] < heights[j]) {
+                    answer[i] = j+1;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
 }
