2de7ff3c7ad0131d84dbf83d6b0904cb78b2f159
kkssry <kkssry@naver.com>
2019-03-24 13:44:33 +0900

    2주차 문제풀이

--- src/main/java/programmers/school/week2/완주하지못한선수.java
+++ src/main/java/programmers/school/week2/완주하지못한선수.java
@@ -1,4 +1,37 @@
 package programmers.school.week2;
 
+import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+
 public class 완주하지못한선수 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        Map<String,Integer> map = new HashMap<>();
+        for (int i = 0; i < participant.length; i++) {
+            if (map.containsKey(participant[i])) {
+                int add = map.get(participant[i])+1;
+                map.put(participant[i],add);
+                continue;
+            }
+            map.put(participant[i],1);
+        }
+
+        for (int i = 0; i < completion.length; i++) {
+            if (map.containsKey(completion[i])) {
+                int sub = map.get(completion[i]) - 1;
+                map.put(completion[i],sub);
+            }
+        }
+
+        for (int i = 0; i < participant.length; i++) {
+            if (map.get(participant[i]) == 1) {
+                answer = participant[i];
+                break;
+            }
+        }
+
+        return answer;
+    }
 }
--- src/test/java/programmers/school/week2/완주하지못한선수Test.java
+++ src/test/java/programmers/school/week2/완주하지못한선수Test.java
@@ -1,5 +1,28 @@
-import static org.junit.Assert.*;
+package programmers.school.week2;
+
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class 완주하지못한선수Test {
+    @Test
+    public void mainTest() {
+        완주하지못한선수 sol = new 완주하지못한선수();
 
+        String[] participants = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"stanko", "ana", "mislav"};
+        assertThat(sol.solution(participants, completion)).isEqualTo("mislav");
+    }
+
+    @Test
+    public void 배열테스트() {
+        String[] arr = {"a","b","c","d"};
+        String[] arr2 = {"a","b","c"};
+        List<Integer> list = new ArrayList<>();
+    }
 }
