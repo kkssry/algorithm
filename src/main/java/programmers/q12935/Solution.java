e6c5a1c6a4dceec3f149e71191ac6e2d934f7353
kkssry <kkssry@naver.com>
2019-03-24 13:42:17 +0900

    미해결 문제(제일작은수 제거하기)

--- src/main/java/programmers/q12935/Solution.java
+++ src/main/java/programmers/q12935/Solution.java
@@ -1,29 +1,29 @@
 package programmers.q12935;
 
-import java.util.PriorityQueue;
+import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Solution {
     public int[] solution(int[] arr) {
-        int[] answer = {-1};
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for (int i1 : arr) {
-            queue.add(i1);
+        int[] answer = {};
+
+        Integer[] numberBox = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(numberBox));
+
+        Collections.sort(numbers, (o1, o2) -> o2 - o1);
+
+        numbers.remove(numbers.get(numbers.size() - 1));
+        if (numbers.isEmpty()) {
+            return new int[]{-1};
         }
 
-        queue.poll();
-
-        if (queue.isEmpty()) {
-            return answer;
-        }
-
-        answer = new int[queue.size()];
-
-        for (int i = answer.length - 1; i > -1; --i) {
-            answer[i] = queue.peek();
-            queue.poll();
+        answer = new int[numbers.size()];
+        for (int i = 0; i < numbers.size(); i++) {
+            answer[i] = numbers.get(i);
         }
 
         return answer;
-
     }
 }
--- src/test/java/programmers/q12935/SolutionTest.java
+++ src/test/java/programmers/q12935/SolutionTest.java
@@ -2,14 +2,13 @@ package programmers.q12935;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class SolutionTest {
     @Test
     public void mainTest() {
         Solution sol = new Solution();
-        int[] arr = {7,4,7,3,1,3,2,-4,-9};
+        int[] arr = {5,345,1,45,3,4,64,74};
         System.out.println(Arrays.toString(sol.solution(arr)));
     }
 
@@ -23,5 +22,45 @@ public class SolutionTest {
         a[1] = queue.poll();
         System.out.println(queue);
         System.out.println(Arrays.toString(a));
+    }
+
+    @Test
+    public void stringBuilder테스트() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(5);
+        sb.append(4);
+        sb.append(3);
+        sb.append(2);
+        sb.append(1);
+        String a = "12345";
+
+        String reverse = sb.reverse().toString();
+        System.out.println(reverse);
+    }
+
+    @Test
+    public void substringTest() {
+        String a = "1";
+        String b = a.substring(0,a.length()-1);
+        if (b.isEmpty()) {
+            System.out.println("비었네");
+        }
+    }
+
+    @Test
+    public void arrayElementDecending() {
+        int[] num = {76,4,5,5,7,3};
+        Integer[] s = Arrays.stream(num).boxed().toArray(Integer[]::new);
+        Integer[] arr = s;
+        List<Integer> list = Arrays.asList(arr);
+        System.out.println(list);
+    }
+
+    @Test
+    public void listRemoveTest() {
+        List<Integer> list= new ArrayList<>(Arrays.asList(1,2,3,4));
+        list.remove(3);
+        System.out.println(list);
+
     }
 }
