e713d8b1fe727a790c1f1395cf5e34175834e87a
kkssry <kkssry@naver.com>
2019-03-25 13:05:48 +0900

    조이스틱(탐욕) 해결중

--- src/main/java/programmers/q42860/Solution.java
+++ src/main/java/programmers/q42860/Solution.java
@@ -1,4 +1,83 @@
 package programmers.q42860;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        int nowIndex = 0;
+
+        char[] words = name.toCharArray();
+        List<Word> list = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            list.add(new Word(words[i], i, words.length - i));
+        }
+
+        System.out.println(list);
+
+
+        while (list.stream().filter(i -> i.isA).count() != list.size()) {
+           loop: for (int i = nowIndex; i < list.size()-1; i++) {
+                if (!list.get(nowIndex).isA) {
+                    nowIndex = list.get(nowIndex).fromFirst;
+                    answer += nowIndex;
+                    answer += alphatCount(list.get(nowIndex).name);
+                    System.out.println("카운트 : " + alphatCount(list.get(nowIndex).name));
+                    list.get(nowIndex).isA = true;
+                    System.out.println(list.get(nowIndex));
+                    break loop;
+                }
+            }
+
+            loop2 :for (int j = list.size() - 1; j >=0; j--) {
+                if (!list.get(j).isA) {
+                    answer += nowIndex - list.get(j).fromFirst;
+                    answer += alphatCount(list.get(j).name);
+                    list.get(j).isA = true;
+                    nowIndex = list.get(j).fromFirst;
+                    System.out.println(answer);
+                    break loop2;
+                }
+            }
+
+        }
+
+        return answer;
+    }
+
+    private int alphatCount(char word) {
+        if (word <= 'N') {
+            return word  - 'A';
+        }
+        return ('Z' - word) + 1;
+    }
+
+    class Word {
+        boolean isA = true;
+        char name;
+        int fromFirst;
+        int fromBack;
+
+        public Word(char name, int fromFirst, int fromBack) {
+            if (name != 'A') {
+                isA = false;
+            }
+            this.name = name;
+            this.fromFirst = fromFirst;
+            this.fromBack = fromBack;
+        }
+
+        @Override
+        public String toString() {
+            return "Word{" +
+                    "isA=" + isA +
+                    ", name=" + name +
+                    ", fromFirst=" + fromFirst +
+                    ", fromBack=" + fromBack +
+                    '}';
+        }
+    }
+
 }
--- src/test/java/programmers/q42860/SolutionTest.java
+++ src/test/java/programmers/q42860/SolutionTest.java
@@ -1,5 +1,70 @@
-import static org.junit.Assert.*;
+package programmers.q42860;
+
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class SolutionTest {
+    @Test
+    public void mainTest() {
+        Solution sol = new Solution();
+        assertThat(sol.solution("JEROEN")).isEqualTo(23);
+    }
 
+    @Test
+    public void 문자열비고() {
+        char n = 'N';
+        char z = 'Z';
+        char a = 'A';
+        char o = 'O';
+
+//        String arr = "abcdefg";
+//        char[] arr2 = arr.toCharArray();
+//        for (int i = 0; i < arr.length(); i++) {
+//            System.out.println(arr2[i]);
+//        }
+
+        System.out.println(n-a);
+        System.out.println(z-n);
+        System.out.println(z-z);
+    }
+
+    @Test
+    public void 나누기테스트() {
+        List<Word> list = new ArrayList<>();
+        list.add(new Word("a"));
+        list.add(new Word("b"));
+        list.add(new Word("c"));
+        list.get(0).isA = true;
+        System.out.println(changeElement(list));
+    }
+
+    class Word{
+        String name;
+        boolean isA;
+
+        public Word(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Word{" +
+                    "name='" + name + '\'' +
+                    ", isA=" + isA +
+                    '}';
+        }
+    }
+
+    private List<Word> changeElement(List<Word> list) {
+        if (list.get(0).equals("a")) {
+            list.get(0).isA = true;
+        }
+        return list;
+    }
 }
