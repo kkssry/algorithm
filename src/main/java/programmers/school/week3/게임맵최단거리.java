1c3501016a4833a2e70b55bdf3ba91eb3bf03c32
kkssry <kkssry@naver.com>
2019-04-07 22:47:47 +0900

    3주차 게임맵최단거리(시간초과)

--- src/main/java/programmers/school/week3/게임맵최단거리.java
+++ src/main/java/programmers/school/week3/게임맵최단거리.java
@@ -1,11 +1,75 @@
 package programmers.school.week3;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class 게임맵최단거리 {
+    Set<Integer> shortestPath = new TreeSet<>();
+
     public int solution(int[][] maps) {
         int answer = 0;
 
+        boolean[][] bol = new boolean[maps.length][maps[0].length]; //왔던길 체크
 
+        recursive(maps, 0, 0, bol, answer);
+        for (int i = 0; i < bol.length; i++) {
+            System.out.println(Arrays.toString(bol[i]));
+        }
+
+        System.out.println(shortestPath);
+        if (shortestPath.isEmpty()) {
+            return -1;
+        }
+        answer = shortestPath.iterator().next();
 
         return answer;
     }
+
+
+    private void recursive(int[][] maps, int row, int column, boolean[][] bol, int answer) {
+
+        if (row == maps.length - 1 && column == maps[0].length - 1) {
+            answer++;
+            shortestPath.add(answer);
+            bol[row][column] = true;
+        }
+
+        if (maps[row][column] == 1 && !bol[row][column]) {
+            answer++;
+            bol[row][column] = true;
+
+            if (row > 0) {
+                if (maps[row - 1][column] == 1 && !bol[row - 1][column]) {    // 위
+                    recursive(maps, row -1, column, bol, answer);
+                }
+            }
+
+            if (column > 0) {
+                if (maps[row][column - 1] == 1 && !bol[row][column - 1]) {    // 왼쪽
+                    recursive(maps, row, column - 1, bol, answer);
+                }
+            }
+
+            if (column < maps[0].length - 1) {
+                if (maps[row][column + 1] == 1 && !bol[row][column + 1]) {     // 오른쪽
+                    recursive(maps, row, column + 1, bol, answer);
+                }
+            }
+
+            if (row < maps.length - 1) {
+                if (maps[row + 1][column] == 1 && !bol[row + 1][column]) {   // 아래
+                    recursive(maps, row + 1, column, bol, answer);
+                }
+            }
+
+
+        }
+
+        bol[row][column] = false;
+
+    }
 }
+
+
+
--- src/test/java/programmers/school/week3/게임맵최단거리Test.java
+++ src/test/java/programmers/school/week3/게임맵최단거리Test.java
@@ -3,13 +3,34 @@ package programmers.school.week3;
 
 import org.junit.Test;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class 게임맵최단거리Test {
     @Test
-    public void Test() {
+    public void test() {
         게임맵최단거리 sol = new 게임맵최단거리();
-        int[][] maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+        int[][] maps = {{1,1,1,1,1},
+                        {1,1,1,1,1},
+                        {1,1,1,1,1},
+                        {1,1,1,1,1},
+                        {1,1,1,1,1}};
         assertThat(sol.solution(maps)).isEqualTo(11);
+    }
+
+    @Test
+    public void treeSet테스트() {
+        Set<Integer> set = new TreeSet<>();
+        set.add(4);
+        set.add(9);
+        set.add(2);
+        set.add(100);
+        set.add(1);
+
+        System.out.println(set.iterator().next());
+
+        boolean[][] bol = new boolean[3][2];
     }
 }
