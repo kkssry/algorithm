fc78859bcad932b246575b867e88b8a81a0dfb81
kkssry <kkssry@naver.com>
2019-04-11 02:12:06 +0900

    숫자게임

--- src/main/java/programmers/q12987/Solution.java
+++ src/main/java/programmers/q12987/Solution.java
@@ -1,66 +1,31 @@
 package programmers.q12987;
 
-
 import java.util.*;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Solution {
+    int answer = 0;
     public int solution(int[] A, int[] B) {
-        int answer = 0;
 
+        Arrays.sort(A);
+        Arrays.sort(B);
 
-        boolean[] bol = new boolean[B.length];
-        recursive(A,B,bol);
+        Stack<Integer> sA = new Stack<>();
+        Stack<Integer> sB = new Stack<>();
+
         for (int i = 0; i < A.length; i++) {
-            if (A[i] < B[i]) {
-                answer++;
-            }
+            sA.push(A[i]);
+            sB.push(B[i]);
         }
 
-        System.out.println(Arrays.toString(B));
+        while(!sA.isEmpty()) {
+            if (sA.peek() < sB.peek()) {
+                sB.pop();
+                answer++;
+            }
+            sA.pop();
+        }
 
         return answer;
     }
-
-    private void recursive(int[] a, int[] b, boolean[] bol) {
-        List<Integer> list = new LinkedList<>();
-        int p = -1;
-        int o = -1;
-
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < b.length; j++) {
-                if (a[i] <= b[j]) {
-                    if (!bol[j]) {
-                        list.add(b[j]);
-//                        System.out.println(b[j]);
-//                        bol[i] = true;
-//                        int tmp = b[j];
-//                        b[j] = b[i];
-//                        b[i] = tmp;
-//                        break;
-                    }
-                }
-            }
-//            bol[i] = true;
-
-            Collections.sort(list);
-            System.out.println(list);
-            for (int j = 0; j < b.length; j++) {
-                if (list.size() > 1) {
-                    if (b[j] == list.get(0)) {
-                        bol[i] = true;
-                        int tmp = b[j];
-                        b[j] = b[i];
-                        b[i] = tmp;
-                    }
-                }
-            }
-            list.clear();
-
-        }
-    }
-
 
 }
--- src/test/java/programmers/q12987/SolutionTest.java
+++ src/test/java/programmers/q12987/SolutionTest.java
@@ -3,20 +3,37 @@ package programmers.q12987;
 
 import org.junit.Test;
 
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class SolutionTest {
     @Test
     public void mainTest() {
         Solution sol = new Solution();
-        int[] a = {5, 1, 3, 7};
-        int[] b = {2, 2, 6, 8};
+        int[] a = {5, 1, 3, 7, 9, 5};
+        int[] b = {2, 2, 6, 8, 10, 3};
 
 
 //        sol.permutation(b,0);
 
 
-        assertThat(sol.solution(a,b)).isEqualTo(3);
+        assertThat(sol.solution(a, b)).isEqualTo(4);
     }
 
+    @Test
+    public void name() {
+        Queue<Integer> pr = new LinkedList<>();
+        pr.add(10);
+        pr.add(7);
+        pr.add(3);
+        pr.add(1);
+        pr.add(2);
+
+        for (int i = pr.size(); i >=0 ; i--) {
+            System.out.println(pr.poll());
+        }
+    }
 }
