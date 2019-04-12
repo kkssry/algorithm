171f23bafa359e22241479425b80217d81268cb2
kkssry <kkssry@naver.com>
2019-04-12 10:11:51 +0900

    정수삼각형

--- src/main/java/programmers/q43105/Solution.java
+++ src/main/java/programmers/q43105/Solution.java
@@ -1,4 +1,67 @@
 package programmers.q43105;
 
+
+import java.util.Arrays;
+
 public class Solution {
+//    int comparisonNum = 0;                            // 주석처리 : 재귀 (but 시간초과)
+
+    public int solution(int[][] triangle) {
+        int answer = 0;
+//        int[] path = new int[triangle.length];
+//
+//        path[0] = triangle[0][0];
+//        recursive(0, triangle, path, 1, 1);
+//        answer = comparisonNum;
+
+        int[][] dp = new int[triangle.length][];
+
+        for (int i = 0; i < triangle.length; i++) {
+            dp[i] = new int[triangle[i].length];
+        }
+
+        dp[0][0] = triangle[0][0];
+
+        for (int i = 1; i < triangle.length; i++) {
+            int floorIndexCount = triangle[i].length;
+            for (int j = 0; j < floorIndexCount; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + triangle[i][j];
+                    continue;
+                }
+
+                if (j == floorIndexCount -1) {
+                    dp[i][j] = dp[i -1][j - 1] + triangle[i][j];
+                    continue;
+                }
+                int num = triangle[i][j];
+                dp[i][j] = dp[i - 1][j -1] <= dp[i - 1][j] ? dp[i - 1][j] + num : dp [i -1][j -1] + num;
+
+            }
+        }
+
+        answer = Arrays.stream(dp[triangle.length-1]).max().getAsInt();
+
+
+        return answer;
+    }
+
+//    private void recursive(int currentIndex, int[][] triangle, int[] path, int depth, int currentFloor) {
+//        if (depth == path.length) {
+//            int sum = Arrays.stream(path).sum();
+//            if (comparisonNum  < sum) {
+//                comparisonNum = sum;
+//            }
+//            return;
+//        }
+//
+//        int nextIndex = currentIndex + 1;
+//        for (int i = currentIndex; i <= nextIndex; i++) {
+//            path[currentFloor] = triangle[currentFloor][i];
+//            recursive(i, triangle, path, depth + 1, currentFloor + 1);
+//
+//        }
+//    }
+
+
 }
--- src/test/java/programmers/q43105/SolutionTest.java
+++ src/test/java/programmers/q43105/SolutionTest.java
@@ -1,5 +1,36 @@
-import static org.junit.Assert.*;
+package programmers.q43105;
+
+
+import org.assertj.core.util.Arrays;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class SolutionTest {
+    @Test
+    public void mainTest() {
+        Solution sol = new Solution();
+        int[][] triangle = {{7}, {3, 8},
+                            {8, 1, 0}, {2, 7, 4, 4},{4,5,2,6,5}
+                            };
+        assertThat(sol.solution(triangle)).isEqualTo(30);
+    }
 
+    @Test
+    public void 배열역정렬() {
+        List<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(4);
+        list.add(8);
+        list.add(12);
+        list.add(5);
+        list.add(9);
+        list.add(10);
+        Collections.sort(list,((o1, o2) -> o2-o1));
+        System.out.println(list);
+    }
 }
