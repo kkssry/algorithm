93146fe324e66cc19a7391ecfa39d4d8e4e1192d
kkssry <kkssry@naver.com>
2019-04-14 14:41:07 +0900

    줄서는방법

--- src/main/java/programmers/q12936/Solution.java
+++ src/main/java/programmers/q12936/Solution.java
@@ -1,43 +1,59 @@
 package programmers.q12936;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Solution {
-    long grade = 0;
-
     public int[] solution(int n, long k) {
-        int[] answer = {};
-        int[] nums = new int[n];
+        int[] answer = new int[n];
+        List<Integer> list = new LinkedList<>();
+        int index = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = i + 1;
+        if (n == 2) {
+            if ((int) k == 1) {
+                return new int[]{1, 2};
+            }
+            return new int[]{2, 1};
         }
 
-        int findfirstIndex = factorial(n-1);
+        long fac = 1;
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+            fac *= i;
+        }
 
-        long firstIndex = k / findfirstIndex;
-        long findRest = k % findfirstIndex;
+        while (k > 0) {
+            fac /= n--;
+            long findFirstIndex = k / fac;
+            k %= fac;
+            if (k == 0) {
+                findFirstIndex--;
+            }
 
-        swap(firstIndex,nums);
+            answer[index] = list.get((int)findFirstIndex);
+            list.remove((int)findFirstIndex);
 
+            index++;
+
+        }
+
+
+        if (k == 0) {
+            Collections.reverse(list);
+        }
+
+//        System.out.println(list);
+
+        for (int i = 0; i < answer.length; i++) {
+            if (answer[i] == 0) {
+                answer[i] = list.get(0);
+                list.remove(0);
+            }
+        }
+
+//        System.out.println(Arrays.toString(answer));
 
         return answer;
     }
-
-    private void swap(long firstIndex, int[] nums) {
-        int tmp = nums[0];
-        nums[0] = nums[(int)firstIndex -1];
-        nums[(int)firstIndex -1] = tmp;
-    }
-
-    private int factorial(int i) {
-        int num = i;
-        for (int j = num-1; j >=1 ; j--) {
-            num *= j;
-        }
-        return num;
-    }
-
 
 }
 
--- src/test/java/programmers/q12936/SolutionTest.java
+++ src/test/java/programmers/q12936/SolutionTest.java
@@ -3,13 +3,76 @@ package programmers.q12936;
 
 import org.junit.Test;
 
+import java.math.BigInteger;
+import java.util.*;
+
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 public class SolutionTest {
     @Test
     public void mainTest() {
         Solution sol = new Solution();
-        int[] arr = {3,1,2};
-        assertThat(sol.solution(3,5)).isEqualTo(arr);
+        int[] arr = {3, 1, 2};
+        assertThat(sol.solution(5, 97)).isEqualTo(arr);
+    }
+
+    @Test
+    public void 리스트부분정렬() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(5, 6, 8, 2, 3, 1, 10));
+
+        int[] arr = {5, 4, 3, 2, 1};
+
+        Collections.sort(list.subList(1, list.size()));
+
+        System.out.println(list);
+    }
+
+    @Test
+    public void 빅인티저테스트() {
+        BigInteger big = BigInteger.ONE;
+        big = big.multiply(BigInteger.valueOf(2));
+        big = big.multiply(BigInteger.valueOf(20));
+        big = big.multiply(BigInteger.valueOf(100));
+        System.out.println(big.intValue());
+    }
+
+    @Test
+    public void integer범위테스트() {
+        BigInteger a = BigInteger.valueOf(20);
+
+        for (long i = 19; i >= 1; i--) {
+            a = a.multiply(BigInteger.valueOf(i));
+        }
+
+        System.out.println(a.toString());
+    }
+
+    @Test
+    public void listAddAllTest() {
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(5, 6, 7, 8));
+
+        list1.addAll(list2);        //list1이 list2의 내용을 흡수함
+
+        System.out.println(list1);
+        System.out.println(list2);
+    }
+
+    @Test
+    public void 인트호출() {
+        int a = 10;
+        int[] b = {1, 2};
+        testMethod(a, b);
+
+        System.out.println(a);
+        System.out.println(Arrays.toString(b));
+    }
+
+    private void testMethod(int a, int[] b) {
+        a -= 4;
+        b[0] = 100;
+
+        System.out.println(a);
+
     }
 }
