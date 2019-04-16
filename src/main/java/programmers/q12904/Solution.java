601433bc2240e76821f7f537912722f7ea446f39
kkssry <kkssry@naver.com>
2019-04-17 01:02:19 +0900

    가장 긴 팰린드롬

--- src/main/java/programmers/q12904/Solution.java
+++ src/main/java/programmers/q12904/Solution.java
@@ -3,8 +3,12 @@ package programmers.q12904;
 public class Solution {
     public int solution(String s) {
         int answer = 1;
+        int length = s.length();        ///         '///' 밑금친거 때문에 효율성 통과...
 
         for (int i = 0; i < s.length() - 1; i++) {
+            if(i==length-answer) {          ///
+                return answer;              ///
+            }                               ///
             int temp;
             for (int j = i + 1; j < s.length(); j++) {
                 if (isPellin(s, i, j)) {
@@ -27,17 +31,17 @@ public class Solution {
     }
 
     private boolean isPellin(String s, int i, int j) {
-        if (s.charAt(i) == s.charAt(j)) {
-            return comparison(s,i,j);
+        if (s.charAt(i) == s.charAt(j) && s.charAt(i + 1) == s.charAt(j - 1)) {
+            return comparison(s, i, j);
         }
         return false;
     }
 
     private boolean comparison(String s, int i, int j) {
-        String extract = s.substring(i,j+1);
+        String extract = s.substring(i, j + 1);
         int length = extract.length();
 
-        for (int k = length - 1; k >= length/2; k--) {
+        for (int k = length - 1; k >= length / 2; k--) {
             if (extract.charAt(k) != extract.charAt(length - k - 1)) {
                 return false;
             }
