69489824b6d5f0d43b734eae2da9dcf24d53c330
kkssry <kkssry@naver.com>
2019-04-26 20:46:50 +0900

    게임맵 최단거리(수정중)

--- src/main/java/programmers/school/week3/게임맵최단거리.java
+++ src/main/java/programmers/school/week3/게임맵최단거리.java
@@ -3,19 +3,46 @@ package programmers.school.week3;
 import java.util.*;
 
 public class 게임맵최단거리 {
-    List<Integer> shortestPath = new LinkedList<>();
 
     public int solution(int[][] maps) {
         int answer = 0;
+        List<Integer> shortestPath = new LinkedList<>();
+
+        Stack<Integer> stack = new Stack<>();
+
+        int mapColumn = maps.length;
+        int mapRow = maps[0].length;
+
+        Direction[] directions = new Direction[4];
+        directions[0] = new Direction(1, 0);     //북
+        directions[1] = new Direction(0, 1);     //동
+        directions[2] = new Direction(-1, 0);    //남
+        directions[3] = new Direction(0, -1);    //서
 
         boolean[][] bol = new boolean[maps.length][maps[0].length]; //왔던길 체크
 
-        recursive(maps, 0, 0, bol, answer);
-        for (int i = 0; i < bol.length; i++) {
-            System.out.println(Arrays.toString(bol[i]));
+//        recursive(maps, 0, 0, bol, answer);
+
+        int initDir = 0;
+
+        Direction cur = new Direction(0, 0);
+        while (true) {
+            if (cur.x == maps.length - 1 && cur.y == maps[0].length - 1) {
+                shortestPath.add(stack.size());
+                break;
+            }
+
+            boolean forwarded = false;
+            for (int dir = initDir; dir < 4; dir++) {
+                if (movable(cur, dir, mapColumn, mapRow)) {
+
+
+                }
+            }
+
         }
 
-        System.out.println(shortestPath);
+
         if (shortestPath.isEmpty()) {
             return -1;
         }
@@ -24,54 +51,82 @@ public class 게임맵최단거리 {
         return answer;
     }
 
-
-    private void recursive(int[][] maps, int row, int column, boolean[][] bol, int answer) {
-
-        if (row == maps.length - 1 && column == maps[0].length - 1) {
-            answer++;
-            shortestPath.add(answer);
-            bol[row][column] = true;
+    private boolean movable(Direction cur, int dir, int mapColumn, int mapRow) {
+        if (cur.x == 0 && dir == 0) {         // 위쪽 벽 일때 북쪽으로 못감
+            return false;
         }
 
-        if (maps[row][column] == 1 && !bol[row][column]) {
-            answer++;
-            bol[row][column] = true;
-
-            if (row < maps.length - 1) {
-                if (maps[row + 1][column] == 1 && !bol[row + 1][column]) {   // 아래
-                    recursive(maps, row + 1, column, bol, answer);
-                }
-            }
-
-            if (row > 0) {
-                if (maps[row - 1][column] == 1 && !bol[row - 1][column]) {    // 위
-                    recursive(maps, row -1, column, bol, answer);
-                }
-            }
-
-            if (column < maps[0].length - 1) {
-                if (maps[row][column + 1] == 1 && !bol[row][column + 1]) {     // 오른쪽
-                    recursive(maps, row, column + 1, bol, answer);
-                }
-            }
-
-
-            if (column > 0) {
-                if (maps[row][column - 1] == 1 && !bol[row][column - 1]) {    // 왼쪽
-                    recursive(maps, row, column - 1, bol, answer);
-                }
-            }
-
+        if (cur.y == mapRow -1 && dir == 1) {       //동쪽 벽일때 동쪽으로 못감
+            return  false;
         }
 
-        bol[row][column] = false;
-        for (int i = 0; i < bol.length; i++) {
-            System.out.println(Arrays.toString(bol[i]));
+        if (cur.x == mapColumn - 1 && dir == 2) {       //남쪽 벽일때 남쪽으로 못감
+            return false;
         }
-        System.out.println("------------");
 
+        if (cur.y == 0 && dir == 3) {         // 서쪽 벽 일때 서쪽으로 못감
+            return false;
+        }
+
+        return true;
+    }
+
+
+//    private void recursive(int[][] maps, int row, int column, boolean[][] bol, int answer) {
+//        if (row == maps.length - 1 && column == maps[0].length - 1) {
+//            answer++;
+//            shortestPath.add(answer);
+//            bol[row][column] = true;
+//        }
+//
+//        if (maps[row][column] == 1 && !bol[row][column]) {
+//            answer++;
+//            bol[row][column] = true;
+//
+//            if (row < maps.length - 1) {
+//                if (maps[row + 1][column] == 1 && !bol[row + 1][column]) {   // 아래
+//                    recursive(maps, row + 1, column, bol, answer);
+//                }
+//            }
+//
+//            if (row > 0) {
+//                if (maps[row - 1][column] == 1 && !bol[row - 1][column]) {    // 위
+//                    recursive(maps, row -1, column, bol, answer);
+//                }
+//            }
+//
+//            if (column < maps[0].length - 1) {
+//                if (maps[row][column + 1] == 1 && !bol[row][column + 1]) {     // 오른쪽
+//                    recursive(maps, row, column + 1, bol, answer);
+//                }
+//            }
+//
+//
+//            if (column > 0) {
+//                if (maps[row][column - 1] == 1 && !bol[row][column - 1]) {    // 왼쪽
+//                    recursive(maps, row, column - 1, bol, answer);
+//                }
+//            }
+//
+//        }
+//
+//        bol[row][column] = false;
+//
+//    }
+}
+
+class Direction {
+    int x;
+    int y;
+
+    public Direction(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
+
+
+
 
 
 
