284572a771a828bce54c586ddbbb9bf1e5c56f0b
kkssry <kkssry@naver.com>
2019-05-07 01:55:00 +0900

    도둑질 수정중

--- src/main/java/programmers/school/week4/도둑질.java
+++ src/main/java/programmers/school/week4/도둑질.java
@@ -1,66 +1,71 @@
 package programmers.school.week4;
 
+
 import java.util.Arrays;
 
 public class 도둑질 {
     public int solution(int[] money) {
         int answer = 0;
+        int length = money.length;
 
-        if (money.length % 2 == 0) {
-            return (int)countEven(money);
+        if (money.length == 3) {
+            int max = -1;
+            for (int i = 0; i < money.length; i++) {
+                if (max < money[i]) {
+                    max = money[i];
+                }
+            }
+            return max;
         }
 
-        if (money.length % 2 == 1) {
-            answer = (int)countOdd(money);
+        long[] case1 = new long[length - 1];        //첫번째 집 방문
+        long[] case2 = new long[length];        //두번째 집 방문
+
+        case1[0] = money[0];
+        case1[1] = 0;
+        case1[2] = money[2] + case1[0];
+
+        for (int i = 3; i < case1.length; i++) {
+            if (i == 3) {
+                case1[i] = money[i] + case1[i - 3];
+            }
+
+            if (i == 4) {
+                case1[i] = case1[i - 2] + money[i];
+                continue;
+            }
+
+            if (i > 4) {
+                case1[i] = Math.max(case1[i - 2], case1[i - 3]) + money[i];
+
+            }
+
         }
+        long case1Max = Math.max(case1[length - 2], case1[length - 3]);
 
-        return answer;
-    }
+        case2[1] = money[1];
+        case2[2] = 0;
+        case2[3] = money[3] + case2[1];
 
-    private long countOdd(int[] money) {
-        long[] dp = new long[4];
-        int moneyCount = money.length;
-        for (int i = 0; i < moneyCount; ++i) {
-            if (i % 2 == 0 && i < moneyCount - 3) {
-                dp[0] += money[i];
-                dp[1] += money[i];
+        for (int i = 4; i < case2.length; i++) {
+            if (i == 4) {
+                case2[i] = case2[i - 3] + money[i];
+                continue;
             }
 
-            if (i == moneyCount - 3) {
-                dp[0] += money[i];
+            if (i == 5) {
+                case2[i] = case2[i - 2] + money[i];
+                continue;
             }
 
-            if (i == moneyCount - 2) {
-                dp[1] += money[i];
-                dp[2] += money[i];
-            }
-
-            if (i % 2 == 1 && i < moneyCount-2) {
-                dp[2] += money[i];
-                dp[3] += money[i];
-            }
-
-            if (i == money.length -1) {
-                dp[3] += money[i];
-            }
-        }
-        System.out.println(Arrays.toString(dp));
-        return Arrays.stream(dp).max().getAsLong();
-    }
-
-
-    private long countEven(int[] money) {
-        long answer;
-        long[] dp = new long[2];
-        for (int i = 0; i < money.length; i++) {
-            if (i % 2 == 0) {
-                dp[0] += money[i];
-            }
-            if (i % 2 == 1) {
-                dp[1] += money[i];
+            if (i > 5) {
+                case2[i] = Math.max(case2[i - 2], case2[i - 3]) + money[i];
             }
         }
-        answer = dp[0] <= dp[1] ? dp[1] : dp[0];
+        System.out.println(Arrays.toString(case2));
+        long case2Max = Math.max(case2[length - 1], case2[length - 2]);
+        answer = (int)Math.max(case1Max,case2Max);
+
         return answer;
     }
 }
--- src/test/java/programmers/school/week4/도둑질Test.java
+++ src/test/java/programmers/school/week4/도둑질Test.java
@@ -2,10 +2,19 @@ package programmers.school.week4;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 
 public class 도둑질Test {
+    @Test
+    public void finalTest() {
+        도둑질 sol = new 도둑질();
+        int[] money = {9,9,9,9,5};
+        System.out.println(sol.solution(money));
+    }
+
     @Test
     public void mainTest() {
         도둑질 sol = new 도둑질();
@@ -13,5 +22,19 @@ public class 도둑질Test {
         int[] arr2 = {8,9,5,14,11,12,10};
 
         assertThat(sol.solution(arr2)).isEqualTo(35);
+    }
+
+    @Test
+    public void arrayCopyTest() {
+        int[] arr = {1,2,3,4,5};
+        int[] arr2 = new int[4];
+
+        System.arraycopy(arr,1,arr2,0,4);
+        System.out.println(Arrays.toString(arr2));
+    }
+
+    @Test
+    public void mathMaxTest() {
+        System.out.println(Math.max(1,1));
     }
 }
