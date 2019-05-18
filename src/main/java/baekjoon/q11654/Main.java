e77002df1323f61c3ab89b33def6119eb045957a
kkssry <kkssry@naver.com>
2019-05-19 00:35:47 +0900

    B 아스키 코드

---
+++ src/main/java/baekjoon/q11654/Main.java
@@ -0,0 +1,14 @@
+package baekjoon.q11654;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        char oneWord = word.charAt(0);
+        System.out.println((int)oneWord);
+    }
+}
