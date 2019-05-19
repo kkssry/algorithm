1e247b9ce00f8581a75fc98df2ad968e624588c9
kkssry <kkssry@naver.com>
2019-05-20 01:46:33 +0900

    B OX퀴즈

---
+++ src/main/java/baekjoon/q8958/Main.java
@@ -0,0 +1,51 @@
+package baekjoon.q8958;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner copy = new Scanner(System.in);
+        int input = Integer.parseInt(copy.next());
+
+        for (int i = 0; i < input; i++) {
+            calculateSum(copy.next());
+        }
+    }
+
+    private static void calculateSum(String correctTable) {
+        String[] corrects = correctTable.split("");
+        int sum = 0;
+        sum += isFirstValueCorrect(corrects[0], sum);
+        sum += calculateDetatilCorrect(corrects);
+        System.out.println(sum);
+    }
+
+    private static int calculateDetatilCorrect(String[] corrects) {
+        int originSum = 0;
+        int one = 1;
+        for (int i = 1; i < corrects.length; i++) {
+            if (corrects[i].equals("X")) {
+                one = 1;
+            }
+
+            if (corrects[i - 1].equals(corrects[i]) && corrects[i].equals("O")) {
+                one += 1;
+                originSum += one;
+                continue;
+            }
+
+            if (corrects[i].equals("O")) {
+                originSum += one;
+            }
+        }
+        return originSum;
+    }
+
+    private static int isFirstValueCorrect(String correct, int currentSum) {
+        if (correct.equals("O")) {
+            currentSum++;
+        }
+        return currentSum;
+    }
+}
