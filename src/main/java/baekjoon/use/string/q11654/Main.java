5167bd50ee8e3eb23c28e12d4b978adcaa9c6123
kkssry <kkssry@naver.com>
2019-05-23 21:34:05 +0900

    B 아스키코드

--- src/main/java/baekjoon/use/string/q11654/Main.java
+++ src/main/java/baekjoon/use/string/q11654/Main.java
@@ -1,9 +1,8 @@
-package baekjoon.q11654;
+package baekjoon.use.string.q11654;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
