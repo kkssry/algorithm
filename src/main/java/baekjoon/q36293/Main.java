b2fd7599601ac004ecbf11a8f31257f1b26cb99e
kkssry <kkssry@naver.com>
2019-05-25 14:17:16 +0900

    B 최소공배수

--- src/main/java/baekjoon/q36293/Main.java
+++ src/main/java/baekjoon/q36293/Main.java
@@ -1,4 +1,35 @@
 package baekjoon.q36293;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        for (int i = 0; i < input; i++) {
+            int[] nums = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            int[] copy = Arrays.copyOf(nums,nums.length);
+            int gcd = gcd(copy);
+            System.out.println(gcd * nums[0] / gcd * nums[1] / gcd);
+        }
+    }
+
+    private static int gcd(int[] nums) {
+        int rest;
+        Arrays.sort(nums);
+        rest = nums[0] % nums[1];
+        while(rest != 0) {
+            nums[0] = nums[1];
+            nums[1] = rest;
+            rest = nums[0] % nums[1];
+        }
+
+        return Math.min(nums[0],nums[1]);
+
+    }
 }
