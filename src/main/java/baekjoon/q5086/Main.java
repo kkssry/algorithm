45af9d58065bd507fecf09d9ecab970daed7f91b
kkssry <kkssry@naver.com>
2019-12-07 23:48:10 +0900

    B 배수와 약수

---
+++ src/main/java/baekjoon/q5086/Main.java
@@ -0,0 +1,26 @@
+package baekjoon.q5086;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        String line;
+//        while (!(line = br.readLine()).equals("0 0")) {
+//            int[] arr = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+//            if (arr[1] % arr[0] == 0) {
+//                bw.write("factor\n");
+//                continue;
+//            }
+//            if (arr[0] % arr[1] == 0) {
+//                bw.write("multiple\n");
+//                continue;
+//            }
+//            bw.write("neither\n");
+//        }
+//        bw.flush();
+
+    }
+}
