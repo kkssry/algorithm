0f1bc4f1ec969f8190a95517488784d057518781
kkssry <kkssry@naver.com>
2019-12-10 22:11:52 +0900

    B 수학은 체육

---
+++ src/main/java/baekjoon/q15894/Main.java
@@ -0,0 +1,9 @@
+package baekjoon.q15894;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Scanner(System.in).nextLong() * 4);
+    }
+}
